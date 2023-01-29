adf059b704d62a93534b5192c01bb0142e4a71ea
Richard Harris <richardharris916@gmail.com>
2023-01-29 16:23:09 -0600

    Updated experience section to be accordion

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Smooth scrolling for the main nav
 let nav = document.querySelectorAll("header nav a");
 for (let link of nav) {
   link.addEventListener("click", () => {
@@ -11,3 +12,25 @@ for (let link of nav) {
     }
   });
 }
+
+// Single-open accordion functionality for jobs
+const jobs = document.querySelectorAll("details.job");
+for (let details of jobs) {
+  details.addEventListener("toggle", (e) => {
+    let open = e.currentTarget.open;
+    e.currentTarget.setAttribute("data-active", open);
+    for (let d of jobs) {
+      if (
+        open &&
+        (d.id !== e.currentTarget.id ||
+          e.currentTarget.dataset.active !== "true") &&
+        (d.open || d.dataset.active !== "true")
+      ) {
+        d.open = false;
+        d.setAttribute("data-active", false);
+      }
+    }
+  });
+}
+
+const projectLinks = document.querySelectorAll(".project .right a");
