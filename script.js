4dc9284f31884c7e5595dbdc397fd668bbacca6e
Richard Harris <richardharris916@gmail.com>
2023-02-04 16:26:22 -0600

    Updated experience back to slider (will need to think again on the navigation, the dot nav seems to not work)

--- script.js
+++ script.js
@@ -14,32 +14,32 @@ for (let link of nav) {
 }
 
 // Single-open accordion functionality for jobs
-const jobs = document.querySelectorAll("details.job");
-for (let details of jobs) {
-  details.addEventListener("toggle", (e) => {
-    let open = e.currentTarget.open;
-    e.currentTarget.setAttribute("data-active", open);
-    for (let d of jobs) {
-      if (
-        open &&
-        (d.id !== e.currentTarget.id ||
-          e.currentTarget.dataset.active !== "true") &&
-        (d.open || d.dataset.active !== "true")
-      ) {
-        d.open = false;
-        d.setAttribute("data-active", false);
-      }
-    }
-  });
-}
+// const jobs = document.querySelectorAll("details.job");
+// for (let details of jobs) {
+//   details.addEventListener("toggle", (e) => {
+//     let open = e.currentTarget.open;
+//     e.currentTarget.setAttribute("data-active", open);
+//     for (let d of jobs) {
+//       if (
+//         open &&
+//         (d.id !== e.currentTarget.id ||
+//           e.currentTarget.dataset.active !== "true") &&
+//         (d.open || d.dataset.active !== "true")
+//       ) {
+//         d.open = false;
+//         d.setAttribute("data-active", false);
+//       }
+//     }
+//   });
+// }
 
-// Generate project nav
-const projectSlider = document.querySelector(".projects .slider + nav");
-const projects = document.querySelectorAll(".project");
-for (let project of projects) {
+// Generate experience nav
+const jobSlider = document.querySelector(".experience .slider + nav");
+const jobs = document.querySelectorAll(".job");
+for (let job of jobs) {
   let li = document.createElement("li");
   let a = document.createElement("a");
-  a.href = `#${project.id}`;
+  a.href = `#${job.id}`;
   li.appendChild(a);
-  projectSlider.appendChild(li);
+  jobSlider.appendChild(li);
 }
